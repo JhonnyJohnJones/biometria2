6b28813ddfc1f084c9ea2282dc2d3d7ae60237e3
Jonathan L <jhonnyjohnjones1.0@gmail.com>
2024-10-16 23:38:48 -0300

    First version

---
+++ site.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import os
+import json
+
+from biometria import auth, authDirect
+
+app = Flask(__name__, template_folder='template')
+
+@app.route("/")
+def homepage():
+    return render_template("main.html")
+
+@app.route("/salvar")
+def salvarTemp():
+    return render_template("salvar.html")
+
+@app.route("/salvar", methods=["POST"])
+def salvar():
+    # if request.method == "POST":
+    nome = request.form.get("nome")
+    digital = request.files["digital"]
+    # print(digital)
+    js = open("static/users.json", "r")
+    usrs = json.loads(js.read())
+    print(usrs)
+    js.close()
+    for user in usrs["users"]:
+        try:
+            if user.nome == nome:
+                return "Nome já usado"
+        except:
+            break
+    digital.save(f"static/temp/{digital.filename}")
+    print(f"Auth: {auth(digital.filename)}")
+    if not auth(digital.filename):
+        js = open("static/users.json", "w")
+        usrs["users"].append({"nome": nome, "digital": digital.filename})
+        js.write(json.dumps(usrs))
+        js.close()
+        os.rename(f"static/temp/{digital.filename}", f"static/saved/{digital.filename}")
+        return "Salvo"
+    else:
+        os.remove(f"static/temp/{digital.filename}")
+        return "Erro"
+    # if request.method == "GET":
+    #     return render_template("salvar.html")
+        
+
+@app.route("/autenticar", methods=["GET", "POST"])
+def autenticar():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        digital = request.files["digital"]
+        digital.save(f"static/temp/{digital.filename}")
+        # print(digital)
+        js = open("static/users.json", "r")
+        usrs = json.loads(js.read())
+        print(usrs)
+        js.close()
+        if authDirect(digital.filename, nome):
+            os.remove(f"static/temp/{digital.filename}")
+            return f"Bem vindo, {nome}"
+        else:
+            os.remove(f"static/temp/{digital.filename}")
+            return "Não te conheço"
+    if request.method == "GET":
+        return render_template("autenticar.html")
+
+
+
+if __name__ == "__main__":
+    app.run() # type: ignore
